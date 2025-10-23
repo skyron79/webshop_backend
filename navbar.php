834d6d8ef99029ea9fb28396e94a304d44891807
Tyron-cyber <71692815+Tyron-cyber@users.noreply.github.com>
2025-10-24 00:18:43 +0200

    Add initial HTML structure and navbar for multiple pages

---
+++ navbar.php
@@ -0,0 +1,37 @@
+
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <script src="https://kit.fontawesome.com/9fe4bfcebd.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="stylesNav.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav nav class="navbar-container">
+        <div class="logo-container">
+            <img class="logo" src="./assets/pictures/images.jpg" alt="">
+        </div>
+        <hr>
+        <div class="links-container">
+         <ul class= "navbar">
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./about.php">About</a></li>
+            <li><a href="./products.php">Products</a></li>
+            <li><a href="./search.php">Search</a></li>
+        </ul>
+
+        <div>
+            <ul class="icons">
+               <button><i class="fa-solid fa-circle-user"></i></button>
+               <button><i class="fa-solid fa-heart"></i></button>
+               <button><i class="fa-solid fa-cart-shopping"></i></button>
+            </ul>
+        </div>
+        </div>
+    </nav>
+</body>
+</html>
